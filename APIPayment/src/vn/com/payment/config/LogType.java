c17304bf40ff8c0db846ec3ee232ce6fdd24c603
nbphuongvd <84196415+nbphuongvd@users.noreply.github.com>
2021-05-14 23:57:42 +0700

    Delete APIPayment directory

--- APIPayment/src/vn/com/payment/config/LogType.java
+++
@@ -1,7 +0,0 @@
-package vn.com.payment.config;
-
-public class LogType {
-	public static final int ERROR 			 = 1;
-	public static final int GW_HANOIBK	 	 = 2;
-	public static final int CHECKPENDING	 = 3;
-}
--- APIPayment/src/vn/com/payment/ultities/IEncryption.java
+++
@@ -1,5 +0,0 @@
-package vn.com.payment.ultities;
-
-public interface IEncryption {
-
-}
